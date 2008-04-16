d71f811caeecf94cd554d6733bb91a170ba3adbd
Andrea Franz <andrea@gravityblast.com>
2008-04-16 19:26:34 +0200

    removed sexy migrations

--- db/migrate/015_create_gallery_importings.rb
+++ db/migrate/015_create_gallery_importings.rb
@@ -1,9 +1,10 @@
 class CreateGalleryImportings < ActiveRecord::Migration
   def self.up
     create_table :gallery_importings do |t|
-      t.integer :gallery_id
-      t.string  :path
-      t.timestamps
+      t.column :gallery_id, :integer
+      t.column :path, :string
+      t.column :created_at, :datetime
+      t.column :updated_at, :datetime
     end
   end
 
