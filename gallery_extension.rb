4ca8c3c9f8889ca07a73b484c6054f2392602415
Andrea Franz <andrea@gravityblast.com>
2008-11-29 02:54:59 +0100

    removed attachment_fu initialization.

--- gallery_extension.rb
+++ gallery_extension.rb
@@ -34,8 +34,7 @@ class GalleryExtension < Radiant::Extension
     #end
   end
   
-  def activate    
-    init_attachment_fu
+  def activate
     init
     tab_options = {:visibility => [:all]}
     Radiant::Config["gallery.gallery_based"] == 'true' ? tab_options[:before] = "Pages" : tab_options[:after] = "Layouts"
@@ -45,20 +44,7 @@ class GalleryExtension < Radiant::Extension
   
   def deactivate
     admin.tabs.remove "Galleries"
-  end
-  
-  def init_attachment_fu
-    Tempfile.class_eval do
-      # overwrite so tempfiles use the extension of the basename.  important for rmagick and image science
-      def make_tmpname(basename, n)
-        ext = nil
-        sprintf("%s%d-%d%s", basename.to_s.gsub(/\.\w+$/) { |s| ext = s; '' }, $$, n, ext)
-      end
-    end            
-    ActiveRecord::Base.send(:extend, Technoweenie::AttachmentFu::ActMethods)
-    Technoweenie::AttachmentFu.tempfile_path = ATTACHMENT_FU_TEMPFILE_PATH if Object.const_defined?(:ATTACHMENT_FU_TEMPFILE_PATH)
-    FileUtils.mkdir_p Technoweenie::AttachmentFu.tempfile_path
-  end    
+  end        
   
   def init
     Page.send(:include, PageExtensionsForGallery, GalleryTags, GalleryItemTags, GalleryItemInfoTags, GalleryLightboxTags)
