1cff7113ab7f4eb5c1f6a87b4fe0328eaf6f03cf
Andrea Franz <andrea@gravityblast.com>
2008-11-29 02:54:31 +0100

    fixed test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,15 @@
+require 'test/unit'
 # Load the the environment
 unless defined? RADIANT_ROOT
   ENV["RAILS_ENV"] = "test"
-  require "#{File.expand_path(File.dirname(__FILE__) + "/../../../../")}/config/boot"
+  case
+  when ENV["RADIANT_ENV_FILE"]
+    require ENV["RADIANT_ENV_FILE"]
+  when File.dirname(__FILE__) =~ %r{vendor/radiant/vendor/extensions}
+    require "#{File.expand_path(File.dirname(__FILE__) + "/../../../../../../")}/config/environment"
+  else
+    require "#{File.expand_path(File.dirname(__FILE__) + "/../../../../")}/config/environment"
+  end
 end
 require "#{RADIANT_ROOT}/test/test_helper"
 
