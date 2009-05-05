ce5dedcfdf4a59c95b8d34777bd4f1cb3fad3eb5
Tom Cowell <tc@eightsquarestudio.com>
2009-05-06 06:24:18 +0800

    adding limit to count of lightbox_tag and class to li
    Signed-off-by: Andrea Franz <andrea@gravityblast.com>

--- lib/gallery_lightbox_tags.rb
+++ lib/gallery_lightbox_tags.rb
@@ -17,7 +17,8 @@ module GalleryLightboxTags
   
   desc %{
     Usage:
-    <pre><code><r:gallery:lightbox [thumb_width='width' thumb_height='height'] [thumb_size='size' thumb_geometry='c84x84'] [image_width='width' image_height='height'] [image_size='size'] [thumbnail='none']/></code></pre>
+    <pre><code><r:gallery:lightbox [thumb_width='width' thumb_height='height'] [thumb_size='size' thumb_geometry='c84x84'] 
+    [image_width='width' image_height='height'] [image_size='size'] [thumbnail='none'] [class='klass' limit='5']/></code></pre>
     Provides a sized image for current gallery item.
     Current size values are: icon, small, medium, large, original }
   tag 'gallery:lightbox' do |tag|    
@@ -25,13 +26,15 @@ module GalleryLightboxTags
     tag.attr["image_size"] = :large unless (tag.attr["image_width"] || tag.attr["image_height"] || tag.attr["image_size"])
     gallery = find_gallery(tag)
     
-    content = %{ <div class="lightbox_gallery_list"><ul> }
-    gallery.items.each do |item| 
+    klass = " class='#{tag.attr['class']}'" if tag.attr['class']
+    content = %{ <div class="lightbox_gallery_list"><ul> }      
+    limit = tag.attr['limit'].to_i > 0 ? tag.attr['limit'].to_i - 1 : gallery.items.length - 1
+    gallery.items[0..limit].each do |item| 
       width, height, size, geometry = tag.attr["thumb_width"], tag.attr["thumb_height"], tag.attr["thumb_size"], tag.attr["thumb_geometry"]
       thumb_path = item.thumb(:width => width, :height => height, :geometry => geometry).public_filename
       width, height, size, geometry = tag.attr["image_width"], tag.attr["image_height"], tag.attr["image_size"], tag.attr["image_geometry"] 
       image_path = item.thumb(:width => width, :height => height, :geometry => geometry).public_filename 
-      li_start_tag = tag.attr["thumbnail"] == 'none' ? '<li>' : %{<li style="background-image: url('#{thumb_path}')">}       
+      li_start_tag = tag.attr["thumbnail"] == 'none' ? "<li#{klass}>" : %{<li style="background-image: url('#{thumb_path}')"#{klass}>}       
       content << %{ #{li_start_tag}     
                     <a href="#{image_path}" rel="lightbox[#{gallery.name.downcase.gsub(/[^-a-z0-9~\s\.:;+=_]/,'')}]" title="#{item.name}">
                     #{item.name}    
